261ca6b3157b9a1c6d8151cd13d12688a14797af
Leonardo200298 <leonardosanchez119@gmail.com>
2023-04-14 20:04:41 -0300

    Feat(practice): practice ending

--- calculator.ts
+++ calculator.ts
@@ -1,12 +1,20 @@
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = number;
 
-
-const calculator = (a:number, b:number, operation: 'multiply' | 'add' | 'divide') =>{  
+const calculator = (a:number, b:number, operation: Operation): Result  =>{  
     if (operation == 'multiply') return a*b;
     if (operation === 'add') return a+b;
     if (operation == 'divide'){
-        if (b === 0) return 'can\'t divide by 0! sorry';
+        if (b === 0) throw new Error('can\'t divide by 0! sorry');
+       
         return a/b;
     }
+    throw new Error("Operation is not valid");
+    
 }
-const result = calculator(3, 0, 'add');
-console.log(result);
+try {
+    const result = calculator(3, 0, 'add');
+    console.log(result);
+} catch (error) {
+    console.log("Something went wrong", error)
+}
--- multiplier.ts
+++ multiplier.ts
@@ -2,4 +2,9 @@ const multiplicator = (a: number, b: number, printText:string) =>{
     console.log(printText, a * b);
 }
 
-multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is: ');
+console.log(process.argv)
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+
+multiplicator(a, b, `Multiplicated ${a} and ${b} and the result is: `);
+
